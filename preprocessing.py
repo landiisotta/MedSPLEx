328f3094eb16ed15dba7722ddb7dd89e549de41b
isotta-landi <isotta.landi2@mssm.edu>
2025-03-15 16:18:34 -0400

    add pattern in front chunk

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,10 @@
 import re
 from collections import namedtuple
+import unicodedata
 
 Chunk = namedtuple('Chunk', ['keyword', 'text'])
+
+
 def chunkizer(text: str, config: dict, nchar=200) -> list[tuple]:
     """
     Returns a list of tuples where each tuple is a chunk of text and associate keyword.
@@ -14,3 +17,16 @@ def chunkizer(text: str, config: dict, nchar=200) -> list[tuple]:
             chunk = text[start:end]
             chunks.append(Chunk(keyword=keyword, text=chunk))
     return chunks
+
+
+def text_preprocessing(chunk, word):
+    """
+    Creates inputs for LLMs. Returns text chunk with word appended at the beginning. 
+    :param chunk: str
+    :param word: str
+    :return: str
+    """
+    preproc_text = f'Word used: {word}' + ' [...] ' + re.sub(r'  +', ' ',
+                                                             unicodedata.normalize('NFKD', chunk.strip().strip(
+                                                                 '.'))).strip() + ' [...]'
+    return preproc_text

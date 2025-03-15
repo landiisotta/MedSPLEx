d28fb754a18c78bdb4b4406e75526190290737e7
Eugenia Alleva <ealleva@macbookpro.mynetworksettings.com>
2025-03-14 21:57:06 -0400

    chukizer function

---
+++ preprocessing.py
@@ -0,0 +1,13 @@
+import re
+from collections import namedtuple
+
+Chunk = namedtuple('Chunk', ['keyword', 'text'])
+def chunkizer(text: str, config: dict, nchar=200) -> list[tuple]:
+    chunks = []
+    for keyword in config.keys():
+        for match in re.finditer(keyword['regex'], text, re.IGNORECASE):
+            start = max(0, match.start() - nchar)
+            end = min(len(text), match.end() + nchar)
+            chunk = text[start:end]
+            chunks.append(Chunk(keyword=keyword, text=chunk))
+    return chunks

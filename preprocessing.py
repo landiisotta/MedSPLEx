e37d0fc77e48e540ba0be3b0764b67e2131fcf6f
Eugenia Alleva <ealleva@macbookpro.mynetworksettings.com>
2025-03-14 22:13:48 -0400

    added info to chunkizer, choose to maintain named tuple

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,9 @@ from collections import namedtuple
 
 Chunk = namedtuple('Chunk', ['keyword', 'text'])
 def chunkizer(text: str, config: dict, nchar=200) -> list[tuple]:
+    """
+    Returns a list of tuples where each tuple is a chunk of text and associate keyword.
+    """
     chunks = []
     for keyword in config.keys():
         for match in re.finditer(keyword['regex'], text, re.IGNORECASE):

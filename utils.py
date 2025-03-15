5567a7b912ab89140afe1a764202ff6f18e5f897
isotta-landi <isotta.landi2@mssm.edu>
2025-03-15 15:14:15 -0400

    func for building examples added

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import dspy
+
 idx_labels_mapping = {0: 'neutral',
                       1: 'stigmatizing',
                       2: 'privileging'}
@@ -5,3 +7,18 @@ idx_labels_mapping = {0: 'neutral',
 labels_idx_mapping = {'neutral': 0,
                       'stigmatizing': 1,
                       'privileging': 2}
+
+
+def _create_examples(df):
+    examples = [
+        dspy.Example(word=ex.pattern, chunk=ex.text_preproc, valence=ex.label_class).with_inputs('chunk', 'word') for ex
+        in df.itertuples()]
+    return examples
+
+
+def _create_sub_examples(df):
+    sub_examples = [dspy.Example(words=[ex.pattern for ex in df.itertuples()],
+                                 chunks=[ex.text_preproc for ex in df.itertuples()],
+                                 valences=[ex.label_class for ex in df.itertuples()])]
+    sub_examples = [x.with_inputs('words', 'chunks') for x in sub_examples]
+    return sub_examples

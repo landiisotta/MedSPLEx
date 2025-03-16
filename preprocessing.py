e1c70c234954d30cbf9680696860ff8732284863
isotta-landi <isotta.landi2@mssm.edu>
2025-03-16 17:32:36 -0400

    boh

--- preprocessing.py
+++ preprocessing.py
@@ -19,7 +19,7 @@ def chunkizer(text: str, config: dict, nchar=200) -> list[tuple]:
     return chunks
 
 
-def text_preprocessing(chunk, word, config):
+def text_preprocessing(chunk, word, config=None):
     """
     Creates inputs for LLMs. Returns text chunk with word appended at the beginning. 
     :param chunk: str
@@ -27,20 +27,21 @@ def text_preprocessing(chunk, word, config):
     :param config: dict
     :return: str
     """
-    word_config = config[word]
-    word_stigmatizing_score = word_config['stigmatizing_score']
-    word_privileging_score = word_config['privileging_score']
-    if word_stigmatizing_score==5:
-        range_text = 'either privileging or neutral. '
-    elif word_privileging_score==5:
-        range_text = 'either stigmatizing or neutral. '
-    else:
-        range_text = 'privileging, stigmatizing or neutral. '
+    if config:
+        word_config = config[word]
+        word_stigmatizing_score = word_config['stigmatizing_score']
+        word_privileging_score = word_config['privileging_score']
+        if word_stigmatizing_score==5:
+            range_text = 'privileging or neutral'
+        elif word_privileging_score==5:
+            range_text = 'stigmatizing or neutral'
+        else:
+            range_text = 'privileging, stigmatizing, or neutral'
 
-    explanation = f"""
-    This word can be {range_text} 
-    """
-    preproc_text = f'Word used: {word}. ' + explanation + ' [...] ' + re.sub(r'  +', ' ',
+        explanation = f""" This word can have a {range_text} meaning."""
+    else:
+        explanation = ''
+    preproc_text = f'Word used: {word}.' + explanation + ' [...] ' + re.sub(r'  +', ' ',
                                                              unicodedata.normalize('NFKD', chunk.strip().strip(
                                                                  '.'))).strip() + ' [...]'
     return preproc_text
